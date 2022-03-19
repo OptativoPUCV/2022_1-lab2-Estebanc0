fc063340220cd869632703269053ed6ff0b96ed1
Your Name <you@example.com>
2022-03-18 22:30:07 -0300

    new try

--- list.c
+++ list.c
@@ -47,7 +47,7 @@ void * nextList(List * list) {
     if(!list->current){
         return NULL;
     }
-    list->current=list->current->next;
+    list->current=list->current->next->data;
     return list->current->data;
     
 }
